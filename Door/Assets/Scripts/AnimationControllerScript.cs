8219ca9ad7b124059a4447f2086217152513c446
Amelia Sayaka Cutler <33025206+ameliasayaka@users.noreply.github.com>
2018-10-03 12:10:59 +0100

    Unity and Animation Fixes
    Animation refined and imported into Unity
    FPS Player Added

---
+++ Door/Assets/Scripts/AnimationControllerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControllerScript : MonoBehaviour {
+
+    public bool hasPlayerOpened;
+    public bool isAlreadyOpen;
+    private bool playerInRange;
+    private Animator monsterDoorAnimation;
+
+    
+
+	// Use this for initialization
+	void Start () {
+        hasPlayerOpened = false;
+        isAlreadyOpen = false;
+        playerInRange = false;
+        monsterDoorAnimation = GetComponent<Animator>();
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        playerInRange = true;
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        playerInRange = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (playerInRange && !isAlreadyOpen)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                monsterDoorAnimation.SetBool("hasPlayerOpened", hasPlayerOpened);
+                isAlreadyOpen = true;
+            }
+ 
+        }
+	}
+}
